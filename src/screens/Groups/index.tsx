cd43ec3513853364a2c099b11f9da9703f99cebe
Bia-source <bia_ferreirads@yahoo.com>
2023-05-27 23:02:13 -0300

    adicionando styled-components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,21 +1,14 @@
-import React, { StyleSheet, Text, View } from "react-native";
+import React, {  Text } from "react-native";
+import { Container, Title } from "./style";
 
 export function Groups(){
    return (
-    <View style={styles.container}>
-        <Text>
+    <Container>
+        <Title>
             Groups
-        </Text>
-    </View>
+        </Title>
+    </Container>
    )
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
   
