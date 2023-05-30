bd01249f3e69db57bf67961a7ab2672604556337
Bia-source <bia_ferreirads@yahoo.com>
2023-05-29 22:22:30 -0300

    criado componente card de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,19 @@
-import React, {  Text } from "react-native";
+import React, { Text } from "react-native";
 import * as S from "./style";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
-export function Groups(){
-   return (
-    <S.Container>
-        <Header showBackButton/>
-    </S.Container>
-   )
+export function Groups() {
+    return (
+        <S.Container>
+            <Header />
+            <Highlight
+                title="Turmas"
+                subtitle="jogue com a sua turma"
+            />
+            <GroupCard title="Galera do Ignite"/> 
+        </S.Container>
+    )
 }
 
-  
