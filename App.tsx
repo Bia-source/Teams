a663159675f552ac15b18d8e8b69e9bdb689344e
Bia-source <bia_ferreirads@yahoo.com>
2023-06-01 01:37:02 -0300

    estrutura tela players

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import theme from "./src/shared/global/theme/index";
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { Groups } from "@screens/Groups";
+import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold})
@@ -15,7 +16,7 @@ export default function App() {
      backgroundColor="transparent"
      translucent
     />
-    { fontsLoaded ? <NewGroups title="ola"/> : <Loading/>}
+    { fontsLoaded ? <Players/> : <Loading/>}
    </ThemeProvider>
   );
 }
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@components/Header";
+import * as S from "./style";
+import { Highlight } from "@components/Highlight";
+
+export function Players() {
+    return (
+        <>
+            <S.Container>
+                <Header showBackButton />
+                <Highlight
+                    title="Nome da turma"
+                    subtitle="adicione a galera e separe os times"
+                />
+            </S.Container>
+        </>
+    )
+}
